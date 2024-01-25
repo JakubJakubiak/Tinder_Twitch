1d6c90e724cfc4eb2a9980380b7074dc08a8e186
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2024-01-25 16:41:16 +0100

    Update TwitchAuthLogin.tsx

--- pages/TwitchAuthLogin.tsx
+++ pages/TwitchAuthLogin.tsx
@@ -1,59 +1,42 @@
-import React from 'react';
-import { View, Button, Alert, Linking } from 'react-native';
+import React, { useState } from 'react';
+import { View, Platform } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const TwitchAuthLogin = () => {
-  const handleTwitchLogin = async () => {
-    try {
-        const response = await fetch('https://nodejs-api-example.vercel.app/api/hello', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ key: 'value' }),
-        });
+  const initialURL = 'https://end-point-small.vercel.app/auth/twitch';
 
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            // Handle the URL or further processing as needed
-        } else {
-            console.error(`Error occurred: ${response.status}`);
-            // Handle error accordingly
-        }
-    } catch (error:any) {
-        console.error(`Login error: ${error.message}`);
-        // Handle the exception and display an appropriate error message
+  const [webViewURL, setWebViewURL] = useState(initialURL);
+
+  const onNavigationStateChange = (navState: { url: React.SetStateAction<string>; loading: any; }) => {
+    console.log(navState.url);
+    setWebViewURL(navState.url);
+
+    if (!navState.loading && Platform.OS === 'android') {
+      console.log('Redirected URL:', navState.url);
+      fetchPageContent(navState.url);
     }
-};
+  };
 
-
-
-  // const handleTwitchLogin = async () => {
-  //   try {
-  //     const response = await fetch('https://nodejs-api-example.vercel.app/api/hello', {
-  //       method: 'POST', 
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({ key: 'value' }),
-  //     });
-    
-  //     if (response.ok) {
-  //       console.log(response);
-  //       const url = await response.text();
-  //       Linking.openURL(url);
-  //     } else {
-  //       Alert.alert('Error', `Error occurred: ${response.status}`);
-  //     }
-  //   } catch (error:any) {
-  //     console.error('Login error:', error.message);
-  //     Alert.alert('Error', 'An error occurred during login.');
-  //   }
-  // };
+  const fetchPageContent = async (url) => {
+    try {
+      const response = await fetch(url);
+      const htmlContent = await response.text();
+      console.log('Page Content:', htmlContent);
+    } catch (error) { 
+      console.error('Error fetching page content:', error);
+    }
+  };
 
   return (
-    <View>
-      <Button title="Sign in with Twitch" onPress={handleTwitchLogin} />
+    <View style={{  width: "100%", height:"100%" }}>
+      <WebView
+        source={{ uri: webViewURL }}
+        onNavigationStateChange={onNavigationStateChange}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        onLoadEnd={() => fetchPageContent(webViewURL)} 
+      />
     </View>
   );
 };
