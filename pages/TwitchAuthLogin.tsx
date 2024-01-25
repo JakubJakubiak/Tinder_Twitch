fa3302425aebcb801fb714117260b55fe6ca596b
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2024-01-25 17:52:42 +0100

    Update TwitchAuthLogin.tsx

--- pages/TwitchAuthLogin.tsx
+++ pages/TwitchAuthLogin.tsx
@@ -1,43 +1,61 @@
 import React, { useState } from 'react';
-import { View, Platform } from 'react-native';
+import { View, Platform, Text, Image } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const TwitchAuthLogin = () => {
   const initialURL = 'https://end-point-small.vercel.app/auth/twitch';
-
   const [webViewURL, setWebViewURL] = useState(initialURL);
 
-  const onNavigationStateChange = (navState: { url: React.SetStateAction<string>; loading: any; }) => {
-    console.log(navState.url);
-    setWebViewURL(navState.url);
-
-    if (!navState.loading && Platform.OS === 'android') {
-      console.log('Redirected URL:', navState.url);
-      fetchPageContent(navState.url);
+  const isJSON = (str: React.SetStateAction<string>) => {
+    try {
+      JSON.parse(str);
+      return true;
+    } catch (e) {
+      return false;
     }
   };
 
-  const fetchPageContent = async (url) => {
+  const onNavigationStateChange = async (navState: { url: React.SetStateAction<string>; loading: any; }) => {
+    console.log(navState.url);
+    setWebViewURL(navState.url);
+
+    console.log('Redirected URL:', navState.url);
+    await fetchPageContent(navState.url);
+
+  };
+
+  const fetchPageContent = async (url: React.SetStateAction<string> | URL | Request) => {
     try {
       const response = await fetch(url);
       const htmlContent = await response.text();
+
+      if (isJSON(htmlContent)) setWebViewURL(htmlContent);
       console.log('Page Content:', htmlContent);
     } catch (error) { 
       console.error('Error fetching page content:', error);
     }
   };
-
+  const webViewURLObject = JSON.parse(webViewURL);
   return (
-    <View style={{  width: "100%", height:"100%" }}>
-      <WebView
-        source={{ uri: webViewURL }}
-        onNavigationStateChange={onNavigationStateChange}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        startInLoadingState={true}
-        onLoadEnd={() => fetchPageContent(webViewURL)} 
-      />
+    webViewURL && isJSON(webViewURL) ?(  
+    <View>
+      <Text>{webViewURLObject.accessToken}</Text>
+      <Text>{webViewURLObject.refreshToken}</Text>
+      <Image source={{ uri: webViewURLObject.image }} style={{ width: 100, height: 100 }} />
     </View>
+    ):
+    (
+      <View style={{ width: "100%", height: "100%" }}>
+        <WebView
+          source={{ uri: webViewURL }}
+          onNavigationStateChange={onNavigationStateChange}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          onLoadEnd={() => fetchPageContent(webViewURL)}
+        />
+      </View>
+    ) 
   );
 };
 
