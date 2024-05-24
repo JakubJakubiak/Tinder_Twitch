9d59be9b36ecc82a2c7b2d4efd7d2c71d95d6e31
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2024-05-24 22:56:22 +0200

    Gui

--- pages/TwitchAuthLogin.tsx
+++ pages/TwitchAuthLogin.tsx
@@ -1,95 +1,90 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Image, TouchableOpacity, StyleSheet, ActivityIndicator, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+  TextInput,
+} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 // import ButoaddLogin from './ButoaddLogin';
 import {UserData, useUserData} from './UserDataContext';
 import * as Linking from 'expo-linking';
-import { authorize } from 'react-native-app-auth';
-import { auth } from "./config/firebase";
-import { signInWithCustomToken, signOut } from "firebase/auth";
+import {authorize, revoke} from 'react-native-app-auth';
+import {auth} from './config/firebase';
+import {signInWithCustomToken, signOut} from 'firebase/auth';
 
-
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 interface Props {
   updateUser: (userData: UserData) => void;
 }
 
-
-const TwitchAuthLogin: React.FC<Props> = ({updateUser}) => {
-  const {userData, setUserData} = useUserData();
+const TwitchAuthLogin = () => {
+  const {setUserData} = useUserData();
   const [isTwitchClicked, setIsTwitchClicked] = useState(false);
-  const [tokenDetails, setTokenDetails] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const config = {
+    issuer: 'https://id.twitch.tv/oauth2',
+    clientId: 'jewvzg4d0egw796dfbn01tnxp6kdd1',
+    redirectUrl: 'https://end-point-small.vercel.app/oauth-callback',
+    scopes: ['user:read:email'],
+    response_type: 'code',
+    skipCodeExchange: true,
+    // revocationEndpoint: 'https://id.twitch.tv/oauth2/revoke',
+  };
 
-  const Tab = createBottomTabNavigator();
-  const Stack = createNativeStackNavigator(); 
+  async function authorizeWithTwitch() {
+    try {
+      const authResult = await authorize(config);
+      // console.log('Authorization successful:', authResult);
 
+      const codeResponse = await fetch(
+        `https://end-point-small.vercel.app/code?authorizationCode=${authResult.authorizationCode}`,
+        {
+          method: 'GET',
+        },
+      );
 
+      const tokenDetails = await codeResponse.json();
 
+      const dateRel = {
+        userData: {
+          userId: tokenDetails.newUser.uid,
+          displayName: tokenDetails.newUser.name,
+          bio: '',
+          broadcaster_type: tokenDetails.newUser.broadcaster_type,
+          created_at: tokenDetails.newUser.created_at,
+          image:
+            tokenDetails.newUser.avatar ??
+            'https://static-cdn.jtvnw.net/jtv_user_pictures/bf561d2d-8287-4b10-8a60-45f8770317b2-profile_image-70x70.png',
+          tokenDetailsAuth: tokenDetails.customToken,
+        },
+      };
 
+      saveTwitchAuthData(dateRel.userData);
+      setUserData(dateRel.userData);
 
-const config = {
-  issuer: 'https://id.twitch.tv/oauth2',
-  clientId: 'jewvzg4d0egw796dfbn01tnxp6kdd1', 
-  redirectUrl: 'https://end-point-small.vercel.app/oauth-callback',
-  scopes: ['user:read:email'], 
-  response_type: 'code',
-  skipCodeExchange: true,
-};
-
-async function authorizeWithTwitch() {
-  try {
-    const authResult = await authorize(config);
-    // console.log('Authorization successful:', authResult);
-    
-    const codeResponse = await fetch(
-      `https://end-point-small.vercel.app/code?authorizationCode=${authResult.authorizationCode}`,
-      {
-        method: 'GET',
-      },
-    );
-
-    const tokenDetails = await codeResponse.json();
-
-
-  const dateRel = {
-    userData: {
-        userId: tokenDetails.newUser.uid,
-        displayName: tokenDetails.newUser.name,
-        bio: "",
-        broadcaster_type: tokenDetails.newUser.broadcaster_type,
-        created_at: tokenDetails.newUser.created_at,
-        image: tokenDetails.newUser.avatar  ?? "https://static-cdn.jtvnw.net/jtv_user_pictures/bf561d2d-8287-4b10-8a60-45f8770317b2-profile_image-70x70.png",
-        tokenDetailsAuth: tokenDetails.customToken,
+      if (tokenDetails) {
+        const userCredential = await signInWithCustomToken(
+          auth,
+          tokenDetails.customToken,
+        );
+        const user = userCredential.user;
+        // console.log('User signed in: ', user);
+        console.log('User signed in');
+        // updateUser();
+      }
+    } catch (error) {
+      console.error('Authorization error:', error);
     }
-};
-
-    saveTwitchAuthData(dateRel.userData);
-    setUserData(dateRel.userData);
-    setTokenDetails(tokenDetails.customToken);
-
-    if (tokenDetails) {
-    const userCredential = await signInWithCustomToken(auth, tokenDetails.customToken);
-    const user = userCredential.user;
-    // console.log('User signed in: ', user);
-    console.log('User signed in');
-    // updateUser();
-
-    }
-
-  } catch (error) {
-    console.error('Authorization error:', error);
   }
-}
-
 
   const saveTwitchAuthData = async (date: Record<string, any>) => {
     try {
@@ -108,145 +103,144 @@ async function authorizeWithTwitch() {
       const value = await AsyncStorage.getItem('@TwitchAuthLoginSave');
       if (value !== null) {
         const userDataObject = JSON.parse(value);
-        setUserData(userDataObject); 
+        setUserData(userDataObject);
         if (userDataObject) {
-          const userCredential = await signInWithCustomToken(auth, userDataObject.tokenDetailsAuth);
-          if(!userCredential) await removeTwitchAuthData();
+          await signInWithCustomToken(auth, userDataObject.tokenDetailsAuth);
         }
-        updateUser(userDataObject);
-        
+        console.log('Twitch authorization data retrieved successfully.');
       } else {
         console.log('No Twitch authorization data to read.');
       }
     } catch (error) {
-      await removeTwitchAuthData()
+      await removeTwitchAuthData();
       console.log(error);
     }
   };
 
   const removeTwitchAuthData = async () => {
     try {
-      await AsyncStorage.removeItem('@TwitchAuthLoginSave');
+      // const value = await AsyncStorage.getItem('@TwitchAuthLoginSave');
+      // if (value !== null) {
+      //   const accessToken = JSON.parse(value);
+      //   console.log(`Access token: ${accessToken.tokenDetailsAuth}`);
+      //   await revoke(config, {
+      //     tokenToRevoke: accessToken.tokenDetailsAuth,
+      //   });
+      // }
+
       await signOut(auth);
+      await AsyncStorage.removeItem('@TwitchAuthLoginSave');
       setUserData(null);
 
       console.log('Twitch authorization data removed successfully.');
     } catch (error) {
-      console.log('An error occurred while removing Twitch authorization data:', error);
+      console.log(
+        'An error occurred while removing Twitch authorization data:',
+        error,
+      );
     }
   };
-  
-
 
   useEffect(() => {
     retrieveTwitchAuthData();
   }, []);
 
+  const handleDeepLink = (event: any) => {
+    const {url} = event;
+    if (url) {
+      console.log('Dane przekazane z przeglądarki:', url);
+    }
+  };
 
+  useEffect(() => {
+    Linking.addEventListener('url', handleDeepLink);
+  });
 
-const handleDeepLink = (event:any) => {
-  const { url } = event;
-  if (url) {
-    console.log('Dane przekazane z przeglądarki:', url);
-  }
-}
+  // <View>
+  //   <View
+  //     style={{
+  //       flexDirection: 'row',
+  //       alignItems: 'center',
+  //       justifyContent: 'flex-end',
+  //       backgroundColor: '#000',
+  //       paddingHorizontal: 10,
+  //       paddingVertical: 5,
+  //     }}>
+  //         <TouchableOpacity
+  //         style={{
+  //           backgroundColor: '#6441A4',
+  //           padding: 10,
+  //           borderRadius: 5,
+  //           margin: 10,
+  //         }}
+  //         onPress={removeTwitchAuthData}>
+  //         <Text
+  //           style={{
+  //             color: '#FFFFFF',
+  //             fontSize: 16,
+  //             fontWeight: 'bold',
+  //             textTransform: 'uppercase',
+  //           }}>
+  //             Logout
+  //         </Text>
+  //       </TouchableOpacity>
+  //     <View>
+  //       <Text>{userData.displayName}</Text>
+  //       <Text>{userData.bio}</Text>
+  //     </View>
 
-useEffect(() => {
-Linking.addEventListener('url', handleDeepLink);
-});
+  //     <Image
+  //       source={{uri: userData.image}}
+  //       style={{width: 50, height: 50, borderRadius: 25, marginLeft: 10}}
+  //     />
+  //   </View>
+  // </View>
 
-
-  return userData ? (
-    
-    <View>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-          backgroundColor: '#000',
-          paddingHorizontal: 10,
-          paddingVertical: 5,
-        }}>
+  return (
+    <>
+      <View style={{width: '100%', height: '100%'}}>
+        {!isTwitchClicked ? (
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: 10,
+            }}>
             <TouchableOpacity
-            style={{
-              backgroundColor: '#6441A4',
-              padding: 10,
-              borderRadius: 5,
-              margin: 10,
-            }}
-            onPress={removeTwitchAuthData}>
-            <Text
               style={{
-                color: '#FFFFFF',
-                fontSize: 16,
-                fontWeight: 'bold',
-                textTransform: 'uppercase',
-              }}>
-                Logout    
-            </Text>
-          </TouchableOpacity>
-        <View>
-          <Text>{userData.displayName}</Text>
-          <Text>{userData.bio}</Text>
-        </View>
-  
-        <Image
-          source={{uri: userData.image}}
-          style={{width: 50, height: 50, borderRadius: 25, marginLeft: 10}}
-        />
+                backgroundColor: '#6441A4',
+                padding: 10,
+                borderRadius: 5,
+                margin: 10,
+              }}
+              onPress={authorizeWithTwitch}>
+              <Text
+                style={{
+                  color: '#444',
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                  textTransform: 'uppercase',
+                }}>
+                Twitch
+              </Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View>
+            <ActivityIndicator size="large" color="#841584" />
+          </View>
+        )}
       </View>
-    </View>
-  ) : (
-    <View style={{width: '100%', height: '100%'}}>
-      {!isTwitchClicked ? (
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-            padding: 10,
-          }}>
-
-          <TouchableOpacity
-            style={{
-              backgroundColor: '#6441A4',
-              padding: 10,
-              borderRadius: 5,
-              margin: 10,
-            }}
-            onPress={authorizeWithTwitch}>
-
-            <Text
-              style={{
-                color: '#444',
-                fontSize: 16,
-                fontWeight: 'bold',
-                textTransform: 'uppercase',
-              }}>
-              Twitch
-            </Text>
-          </TouchableOpacity>
-
-        </View>
-
-      ) : (
-
-      <View>
-      <ActivityIndicator size="large" color="#841584" />
-      </View>
-      )}
-      
-    </View>
+    </>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 20,
   },
   label: {
@@ -254,37 +248,37 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   input: {
-    width: "100%",
+    width: '100%',
     height: 40,
-    borderColor: "gray",
+    borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 16,
     paddingLeft: 8,
     paddingRight: 8,
   },
   button: {
-    backgroundColor: "#302298",
+    backgroundColor: '#302298',
     borderRadius: 20,
     padding: 10,
     margin: 14,
-    width: "78%",
+    width: '78%',
     height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
   },
   loginText: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
     fontSize: 16,
-    alignSelf: "center",
+    alignSelf: 'center',
   },
   welcomeText: {
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 16,
-    textAlign: "center",
+    textAlign: 'center',
   },
   logo: {
     width: 150,
@@ -292,17 +286,17 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   downText: {
-    color: "#331ece",
+    color: '#331ece',
     fontSize: 16,
-    fontWeight: "400",
+    fontWeight: '400',
     marginTop: 10,
   },
   signup: {
-    alignSelf: "flex-start",
-    textDecorationLine: "underline",
-    color: "#331ece",
+    alignSelf: 'flex-start',
+    textDecorationLine: 'underline',
+    color: '#331ece',
     fontSize: 16,
-    fontWeight: "500",
+    fontWeight: '500',
     marginLeft: 5,
     marginTop: 10,
   },
@@ -313,4 +307,3 @@ export default TwitchAuthLogin;
 function async(customToken: any) {
   throw new Error('Function not implemented.');
 }
-
--- pages/config/MainContainer.tsx
+++ pages/config/MainContainer.tsx
@@ -1,73 +1,112 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {View} from 'react-native';
+import {TouchableOpacity, Text, View} from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import MessageScreen from '../MessageList';
 import Pairing from '../Pairing';
 import Chat from '../chat';
+import {UserData, useUserData} from '../UserDataContext';
+
 import TwitchAuthLogin from '../TwitchAuthLogin';
-import {UserData} from '../UserDataContext';
+import {auth} from './firebase';
+import {signOut} from 'firebase/auth';
 
 const home: string = 'Pairing';
 const chat: string = 'Chat';
 
-const Tab = createBottomTabNavigator<AddictionStackParamList>();
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 interface MainContainerProps {}
 
 function MainContainer(props: MainContainerProps) {
-  const [userData, setUserData] = useState<UserData | null>(null);
+  const {userData, setUserData} = useUserData();
 
-  const updateUser = (userData: UserData | null) => {
-    setUserData(userData);
+  const retrieveTwitchAuthData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@TwitchAuthLoginSave');
+      if (value !== null) {
+        const userDataObject = JSON.parse(value);
+        console.log(userDataObject);
+        // setUserData(userDataObject);
+      }
+    } catch (error) {
+      console.error('Authorization error:', error);
+    }
   };
 
+  const removeTwitchAuthData = async () => {
+    try {
+      await signOut(auth);
+      await AsyncStorage.removeItem('@TwitchAuthLoginSave');
+      setUserData(null);
+
+      console.log('Twitch authorization data removed successfully.');
+    } catch (error) {
+      console.log(
+        'An error occurred while removing Twitch authorization data:',
+        error,
+      );
+    }
+  };
 
   return (
     <NavigationContainer>
-      <TwitchAuthLogin updateUser={updateUser} />
-      <Tab.Navigator
-        initialRouteName={home}
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName: string = '';
-            if (route.name === home) {
-              iconName = 'heart';
-            } else if (route.name === "chat") {
-              iconName = 'message';
+      {!userData ? (
+        <Stack.Navigator>
+          <Stack.Screen name="TwitchAuthLogin" component={TwitchAuthLogin}/>
+          
+        </Stack.Navigator>
+      ) : (
+        <Tab.Navigator
+          initialRouteName={home}
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName: string = '';
+              if (route.name === home) {
+                iconName = 'heart';
+              } else if (route.name === 'chat') {
+                iconName = 'message';
+              }
+              return <Entypo name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'white',
+            tabBarLabelStyle: {paddingBottom: 10, fontSize: 10, color: 'white'},
+            tabBarStyle: {backgroundColor: '#6441a5', padding: 10, height: 70},
+          })}>
+          <Tab.Screen
+            name={home}
+            options={{
+              headerStyle: {
+                backgroundColor: '#6441a5',
+              },
+              headerTintColor: '#fff',
+            }}>
+            {props =>
+              userData ? <Pairing {...props} {...userData} /> : <View />
             }
-            return <Entypo name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'white',
-          tabBarLabelStyle: {paddingBottom: 10, fontSize: 10, color: 'white'},
-          tabBarStyle: {backgroundColor: '#6441a5', padding: 10, height: 70},
-        })}>
-        <Tab.Screen name={home} options={{
-          headerStyle: {
-            backgroundColor: '#6441a5',
-          },
-          headerTintColor: '#fff',
-        }}>
-          {props => (userData ? <Pairing {...props} {...userData}  /> : <View />)}
-        </Tab.Screen>
+          </Tab.Screen>
 
-        <Tab.Screen name="chat" options={{headerShown: false}}>
-          {props =>
-            userData ? (
-              <ChatStackScreen {...props} userData={userData} />
-            ) : (
-              <View />
-            )
-          }
-        </Tab.Screen>
-    
-  
-      </Tab.Navigator>
+          <Tab.Screen name="chat" options={{headerShown: false}}>
+            {props =>
+              userData ? (
+                <ChatStackScreen
+                  {...props}
+                  userData={userData}
+                  removeTwitchAuthData={removeTwitchAuthData}
+                />
+              ) : (
+                <View />
+              )
+            }
+          </Tab.Screen>
+        </Tab.Navigator>
+      )}
     </NavigationContainer>
   );
 }
@@ -75,9 +114,13 @@ function MainContainer(props: MainContainerProps) {
 interface ChatStackScreenProps {
   navigation: any;
   userData: UserData;
+  removeTwitchAuthData: () => void;
 }
-
-function ChatStackScreen({navigation, userData}: ChatStackScreenProps) {
+function ChatStackScreen({
+  navigation,
+  userData,
+  removeTwitchAuthData,
+}: ChatStackScreenProps) {
   return (
     <Stack.Navigator>
       <Stack.Screen
@@ -87,6 +130,27 @@ function ChatStackScreen({navigation, userData}: ChatStackScreenProps) {
             backgroundColor: '#6441a5',
           },
           headerTintColor: '#fff',
+          headerRight: () => (
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#6441A4',
+                padding: 10,
+                borderRadius: 5,
+                marginRight: 10,
+              }}
+              onPress={removeTwitchAuthData} // Pass the function reference here
+            >
+              <Text
+                style={{
+                  color: '#FFFFFF',
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                  textTransform: 'uppercase',
+                }}>
+                Logout
+              </Text>
+            </TouchableOpacity>
+          ),
         }}>
         {props => <HomeNoScreen {...props} userData={userData} />}
       </Stack.Screen>
@@ -121,9 +185,7 @@ function HomeNoScreen({navigation, userData}: HomeNoScreenProps) {
       <MessageScreen
         navigation={navigation}
         route={{userId: userData.userId}}
-        onPress={() =>
-          navigation.navigate('Chat', {route: {...userData}})
-        }
+        onPress={() => navigation.navigate('Chat', {route: {...userData}})}
       />
     </View>
   );
