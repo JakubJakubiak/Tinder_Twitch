4c361e8f78cd30621e5ec4f8f9ba323bbb5b7240
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2024-07-06 20:39:50 +0200

    gui

--- pages/config/MainContainer.tsx
+++ pages/config/MainContainer.tsx
@@ -1,19 +1,19 @@
-import React, {useState, useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {TouchableOpacity, Text, View, Image, Animated} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TouchableOpacity, Text, View, Image, Animated } from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import MessageScreen from '../MessageList';
 import Pairing from '../Pairing';
 import Chat from '../chat';
-import {UserData, useUserData} from '../UserDataContext';
+import { UserData, useUserData } from '../UserDataContext';
 
 import TwitchAuthLogin from '../TwitchAuthLogin';
-import {auth} from './firebase';
-import {signOut} from 'firebase/auth';
+import { auth } from './firebase';
+import { signOut } from 'firebase/auth';
 
 const home: string = 'Pairing';
 const chat: string = 'Chat';
@@ -21,11 +21,10 @@ const chat: string = 'Chat';
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-interface MainContainerProps {}
+interface MainContainerProps { }
 
 function MainContainer(props: MainContainerProps) {
-  const {userData, setUserData} = useUserData();
-
+  const { userData, setUserData } = useUserData();
 
   const retrieveTwitchAuthData = async () => {
     try {
@@ -57,7 +56,6 @@ function MainContainer(props: MainContainerProps) {
     retrieveTwitchAuthData();
   }, []);
 
-
   const [showDetails, setShowDetails] = useState(false);
   const [animatedValue] = useState(new Animated.Value(0));
 
@@ -65,7 +63,7 @@ function MainContainer(props: MainContainerProps) {
     setShowDetails(!showDetails);
     Animated.timing(animatedValue, {
       toValue: showDetails ? 0 : 1,
-      duration: 300,
+      duration: 100,
       useNativeDriver: false,
     }).start();
   };
@@ -80,13 +78,13 @@ function MainContainer(props: MainContainerProps) {
     <NavigationContainer>
       {!userData ? (
         <Stack.Navigator>
-          <Stack.Screen name="TwitchAuthLogin" component={TwitchAuthLogin}/>
+          <Stack.Screen name="TwitchAuthLogin" component={TwitchAuthLogin} />
         </Stack.Navigator>
       ) : (
         <Tab.Navigator
           initialRouteName={home}
-          screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
               let iconName: string = '';
               if (route.name === home) {
                 iconName = 'heart';
@@ -97,64 +95,46 @@ function MainContainer(props: MainContainerProps) {
             },
             tabBarActiveTintColor: 'tomato',
             tabBarInactiveTintColor: 'white',
-            tabBarLabelStyle: {paddingBottom: 10, fontSize: 10, color: 'white'},
-            tabBarStyle: {backgroundColor: '#6441a5', padding: 10, height: 70},
+            tabBarLabelStyle: { paddingBottom: 10, fontSize: 10, color: 'white' },
+            tabBarStyle: { backgroundColor: '#6441a5', padding: 10, height: 70 },
           })}>
           <Tab.Screen
             name={home}
             options={{
-              headerStyle: {
-                backgroundColor: '#6441a5',
-              },
+              headerStyle: { backgroundColor: '#6441a5' },
               headerTintColor: '#fff',
               headerRight: () => (
-                <>
-        <View>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-        }}
-      >
-        <TouchableOpacity onPress={handleImagePress}>
-          <Image
-            source={{ uri: userData.image }}
-            style={{ width: 40, height: 40, borderRadius: 25 }}
-          />
-        </TouchableOpacity>
-      </View>
-      <Animated.View style={[animatedStyle, { position: 'absolute', top: 50, right:2}]}>
-        {showDetails && (
-          <View style={{ backgroundColor: 'white', padding: 10, borderRadius:5 }}>
-            <Text style={{
-                  color: '#000',
-                  padding: 4,
-            }}>{userData.displayName}</Text>
-            <TouchableOpacity
-              style={{
-                backgroundColor: '#6441A4',
-                borderRadius: 5,
-                padding:5,
-              }}
-              onPress={removeTwitchAuthData}
-            >
-              <Text
-                style={{
-                  color: '#FFFFFF',
-                  fontSize: 16,
-                  fontWeight: 'bold',
-                  textTransform: 'uppercase',
-                }}
-              >
-                Logout
-              </Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      </Animated.View>
-    </View>
-                </>
+                <View>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      justifyContent: 'flex-end',
+                    }}
+                  >
+                    <TouchableOpacity onPress={handleImagePress}>
+                      <Image
+                        source={{ uri: userData.image }}
+                        style={{ width: 40, height: 40, borderRadius: 25 }}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                  <Animated.View style={[animatedStyle, { position: 'absolute', top: 50, right: 2 }]}>
+                    {showDetails && (
+                      <View style={{ backgroundColor: 'white', padding: 10, borderRadius: 5 }}>
+                        <Text style={{ color: '#000', padding: 4 }}>{userData.displayName}</Text>
+                        <TouchableOpacity
+                          style={{ backgroundColor: '#6441A4', borderRadius: 5, padding: 5 }}
+                          onPress={removeTwitchAuthData}
+                        >
+                          <Text style={{ color: '#FFFFFF', fontSize: 16, fontWeight: 'bold', textTransform: 'uppercase' }}>
+                            Logout
+                          </Text>
+                        </TouchableOpacity>
+                      </View>
+                    )}
+                  </Animated.View>
+                </View>
               ),
             }}>
             {props =>
@@ -162,16 +142,13 @@ function MainContainer(props: MainContainerProps) {
             }
           </Tab.Screen>
 
-          <Tab.Screen name="chat" options={{headerShown: false}}>
+          <Tab.Screen name="chat" options={{ headerShown: false }}>
             {props =>
               userData ? (
                 <ChatStackScreen
                   {...props}
                   userData={userData}
                   removeTwitchAuthData={removeTwitchAuthData}
-                  handleImagePress={handleImagePress}
-                  animatedValue={animatedValue}
-                  showDetails={showDetails}
                 />
               ) : (
                 <View />
@@ -189,25 +166,70 @@ interface ChatStackScreenProps {
   userData: UserData;
   removeTwitchAuthData: () => void;
 }
+
 function ChatStackScreen({
   navigation,
   userData,
   removeTwitchAuthData,
 }: ChatStackScreenProps) {
+  const [showDetails, setShowDetails] = useState(false);
+  const [animatedValue] = useState(new Animated.Value(0));
+
   
+  const handleImagePress = () => {
+    setShowDetails(!showDetails);
+    Animated.timing(animatedValue, {
+      toValue: showDetails ? 0 : 1,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const animatedStyle = {
+    opacity: animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+    }),
+  };
+
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="Home"
         options={{
-          headerStyle: {
-            backgroundColor: '#6441a5',
-          },
+          headerStyle: { backgroundColor: '#6441a5' },
           headerTintColor: '#fff',
-          headerRight: () => (
-        <>
-                </>
-          ),
+          headerRight: () => <View>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+            }}
+          >
+            <TouchableOpacity onPress={handleImagePress}>
+              <Image
+                source={{ uri: userData.image }}
+                style={{ width: 40, height: 40, borderRadius: 25 }}
+              />
+            </TouchableOpacity>
+          </View>
+          <Animated.View style={[animatedStyle, { position: 'absolute', top: 50, right: 2 }]}>
+            {showDetails && (
+              <View style={{ backgroundColor: 'white', padding: 10, borderRadius: 5 }}>
+                <Text style={{ color: '#000', padding: 4 }}>{userData.displayName}</Text>
+                <TouchableOpacity
+                  style={{ backgroundColor: '#6441A4', borderRadius: 5, padding: 5 }}
+                  onPress={removeTwitchAuthData}
+                >
+                  <Text style={{ color: '#FFFFFF', fontSize: 16, fontWeight: 'bold', textTransform: 'uppercase' }}>
+                    Logout
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          </Animated.View>
+        </View>
         }}>
         {props => <HomeNoScreen {...props} userData={userData} />}
       </Stack.Screen>
@@ -215,9 +237,7 @@ function ChatStackScreen({
         name="Chat"
         component={Chat}
         options={{
-          headerStyle: {
-            backgroundColor: '#6441a5',
-          },
+          headerStyle: { backgroundColor: '#6441a5' },
           headerTintColor: '#fff',
         }}
       />
@@ -230,7 +250,7 @@ interface HomeNoScreenProps {
   userData: UserData;
 }
 
-function HomeNoScreen({navigation, userData}: HomeNoScreenProps) {
+function HomeNoScreen({ navigation, userData }: HomeNoScreenProps) {
   return (
     <View
       style={{
@@ -241,8 +261,8 @@ function HomeNoScreen({navigation, userData}: HomeNoScreenProps) {
       }}>
       <MessageScreen
         navigation={navigation}
-        route={{userId: userData.userId}}
-        onPress={() => navigation.navigate('Chat', {route: {...userData}})}
+        route={{ userId: userData.userId }}
+        onPress={() => navigation.navigate('Chat', { route: { ...userData } })}
       />
     </View>
   );
